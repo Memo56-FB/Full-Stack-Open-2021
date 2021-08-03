528d499fe8733993da2a0f54460605615ccc144f
Guillermo Farfan <memo56_fb@hotmail.com>
2021-08-03 12:14:29 -0500

    exercise 1.1

---
+++ part1/src/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Total(props) {
+    return (
+        <p>
+            Numbers of exercises: {props.exercises1 + props.exercises2 + props.exercises3}  
+        </p>
+    )
+}
