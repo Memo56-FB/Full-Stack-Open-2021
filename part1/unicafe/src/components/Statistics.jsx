41aef125506c61b52366c931e979b5988cbe2f4d
Guillermo Farfan <memo56_fb@hotmail.com>
2021-08-06 11:04:15 -0500

    exercise 1.11

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -14,12 +14,16 @@ export const Statistics = (props) => {
     return (
         <>
             <h2>Statistics</h2>
-            <Statistic text="Good" value={good}/>
-            <Statistic text="Neutral" value={neutral}/>
-            <Statistic text="Bad" value={bad}/>
-            <Statistic text="All" value={all}/>
-            <Statistic text="Average" value={all > 0 && average / all}/>
-            <Statistic text="Positive" value={all > 0 && <>{good / all * 100} %</>}/>   
+            <table>
+                <tbody>
+                    <Statistic text="Good" value={good}/>
+                    <Statistic text="Neutral" value={neutral}/>
+                    <Statistic text="Bad" value={bad}/>
+                    <Statistic text="All" value={all}/>
+                    <Statistic text="Average" value={all > 0 && average / all}/>
+                    <Statistic text="Positive" value={all > 0 && <>{good / all * 100} %</>}/>  
+                </tbody> 
+            </table>
         </>
     )
 }
