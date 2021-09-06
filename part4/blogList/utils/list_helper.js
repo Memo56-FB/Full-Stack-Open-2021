dea7257de31b589cb9d671888d67f34cdca6e7a2
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-06 11:39:06 -0500

    exercise 4.4

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,6 +1,14 @@
 const dummy = blogs =>{
     return 1
 }
+const totalLikes = blogs => {
+    if(blogs.length === 0){
+        return 0
+    }
+    const likesArray = blogs.map(blog => blog.likes)
+    const result = likesArray.reduce((a,b)=>a+b)
+    return result
+}
 module.exports = {
-    dummy
+    dummy,totalLikes
 }
