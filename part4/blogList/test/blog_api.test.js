fcfb812c46eabde657b315a76046b544847776fb
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-09 12:02:41 -0500

    exercise 4.9

--- part4/blogList/test/blog_api.test.js
+++ part4/blogList/test/blog_api.test.js
@@ -10,6 +10,10 @@ test("notes are returned as json", async () => {
         .expect(200)
         .expect("Content-Type", "application/json; charset=utf-8")
 })
+test("contain id",async () => {
+    const response = await api.get("/api/blogs")
+    expect(response.body.map(body => body.id)).toBeDefined()
+})
 
 afterAll(()=>{
     mongoose.connection.close()
