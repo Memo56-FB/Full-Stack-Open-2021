322fbed891b96a3399db155ad83b901dbd65c26d
Guillermo Farfan <memo56_fb@hotmail.com>
2021-10-06 11:54:39 -0500

    exercise 4.20 with other solution

--- part4/blogList/utils/middleware.js
+++ part4/blogList/utils/middleware.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const logger = require('./logger')
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -26,8 +28,34 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const userExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  let token = null
+
+  if (authorization && authorization.toLocaleLowerCase().startsWith('bearer ')) {
+    token = authorization.substring(7)
+  }
+
+  let decodedToken = {}
+
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET)
+  } catch (error) {}
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const userId = decodedToken.id
+
+  request.userId = userId
+
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  userExtractor
 }
